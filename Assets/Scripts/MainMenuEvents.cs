2ad5a2aa1dae2f7c1512dcea1ec91ae4121fc177
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2025-09-09 19:03:57 +0300

    Added transition effects in menu

--- Assets/Scripts/MainMenuEvents.cs
+++ Assets/Scripts/MainMenuEvents.cs
@@ -1,46 +1,75 @@
 using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class MainMenuEvents : MonoBehaviour
 {
     private UIDocument _document;
-    private Button _button;
+    private Button _btnStart, _btnSettings, _btnExit;
     //private AudioSource _audioSource; // uncomment when there's audio
     private List<Button> _menuButtons;
     private void Awake()
     {
         //_audioSource = GetComponent<AudioSource>();
         _document = GetComponent<UIDocument>();
-        _button = _document.rootVisualElement.Q<Button>("Start");
-        _button.RegisterCallback<ClickEvent>(OnPlayGameClick);
+    }
+    private void OnEnable()
+    {
+        _btnStart = _document.rootVisualElement.Q<Button>("btnStart");
+        _btnStart.clicked += OnStartGameClick;
+        _btnSettings = _document.rootVisualElement.Q<Button>("btnSettings");
+        _btnSettings.clicked += OnSettingsClick;
+        _btnExit = _document.rootVisualElement.Q<Button>("btnExit");
+        _btnExit.clicked += OnExitClick;
+
         _menuButtons = _document.rootVisualElement.Query<Button>().ToList();
 
         for (int i = 0;  i < _menuButtons.Count; i++)
         {
-            _menuButtons[i].RegisterCallback<ClickEvent>(OnAllButtonsClick);
+            _menuButtons[i].clicked += OnAllButtonsClick;
+        }
+        
+    }
+
+    private void OnDisable()
+    {
+        _btnStart.clicked -= OnStartGameClick;
+        _btnSettings.clicked -= OnSettingsClick;
+        _btnExit.clicked -= OnExitClick;
+
+        for (int i = 0; i < _menuButtons.Count; i++)
+        {
+            _menuButtons[i].clicked -= OnAllButtonsClick;
         }
     }
 
-    private void OnAllButtonsClick(ClickEvent evt)
+    private void OnExitClick()
+    {
+        //throw new NotImplementedException();
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+
+    private void OnSettingsClick()
+    {
+        //throw new NotImplementedException();
+        Debug.Log("Settings yo");
+
+    }
+
+    private void OnAllButtonsClick()
     {
         Debug.Log("meow");
         //_audioSource.Play();
         //throw new NotImplementedException();
     }
-
-    private void OnDisable()
-    {
-        _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
-        for( int i = 0; i < _menuButtons.Count; i++)
-        {
-            _menuButtons[i].UnregisterCallback<ClickEvent>(OnAllButtonsClick);
-        }
-    }
-    private void OnPlayGameClick(ClickEvent evt)
+    private void OnStartGameClick()
     {
         Debug.Log("Play game lol");
-        //throw new NotImplementedException();
+        //gameObject.SetActive(false); // straight to the game
     }
 }
