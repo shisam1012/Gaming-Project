b6ded3b2ec8e32a6006c507a14f92d36f03c14c6
shisam1012 <shsa10121999@gmail.com>
2025-09-14 10:59:47 +0300

    fix: add navigation to main menu from win screen

--- Assets/Scripts/GameWinScreen.cs
+++ Assets/Scripts/GameWinScreen.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameWinScreen : MonoBehaviour
 {
    public void SetUp()
     {
         gameObject.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 }
